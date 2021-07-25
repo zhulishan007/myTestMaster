c45470f9ec7094e9405cec306444e472d2eaaf33
zhulishan <zhulishan007@163.com>
2021-07-25 21:05:32 +0800

    Master

--- myGitTest/Program.cs
+++ myGitTest/Program.cs
@@ -6,6 +6,7 @@ namespace myGitTest
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Send to Master");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
             Console.ReadKey();
