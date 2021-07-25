db832fa365fd308b8e19a53019c64fba68097fce
zhulishan <zhulishan007@163.com>
2021-07-25 14:18:40 +0800

    初始化

---
+++ myGitTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myGitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
