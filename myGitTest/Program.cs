ce04711946d88327f9d7534fa37b450a9e131ed7
zhulishan <zhulishan007@163.com>
2021-07-25 14:21:38 +0800

    测试

--- myGitTest/Program.cs
+++ myGitTest/Program.cs
@@ -7,6 +7,8 @@ namespace myGitTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
         }
     }
 }
