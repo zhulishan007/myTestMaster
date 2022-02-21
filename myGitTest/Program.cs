51fab917d9748368bbc65c2e99d9632a1c74a91c
zhulishan007 <zhulishan007@163.com>
2022-02-21 23:03:29 +0800

    初始化

---
+++ myGitTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace myGitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Send to Master1");
+            Console.WriteLine("Send to Master2");
+            Console.WriteLine("Send to Master");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("----------Send to Master");
+            Console.WriteLine("----------Send to Master");
+
+            Console.WriteLine("========Send to Master");
+            Console.WriteLine("========Send to Master");
+            Console.ReadKey();
+        }
+    }
+}
