630cbe6dec2a403d97d91b1e2b13c35ddbcc424e
zhulishan007 <zhulishan007@163.com>
2022-02-26 01:09:24 +0800

    临时提交

--- myGitTest/Program.cs
+++ myGitTest/Program.cs
@@ -6,6 +6,8 @@ namespace myGitTest
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("YYYYYYYYYYY");
+            Console.WriteLine("YYYYYYYYYYY");
             Console.WriteLine("ddddddddd");
             Console.WriteLine("XXXXXXXXXXXX");
             Console.WriteLine("XXXXXXXXXXXX");
