9567db3955ec30bd2bdce21ccff271f9217fa6d8
zhulishan007 <zhulishan007@163.com>
2022-02-26 01:48:01 +0800

    t

--- myGitTest/Program.cs
+++ myGitTest/Program.cs
@@ -17,6 +17,16 @@ namespace myGitTest
             Console.WriteLine("========Send to Master");
             Console.WriteLine("========Send to Master");
             Console.ReadKey();
+            Console.WriteLine("Send to Master1");
+            Console.WriteLine("Send to Master2");
+            Console.WriteLine("Send to Master");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("----------Send to Master");
+            Console.WriteLine("----------Send to Master");
+
+            Console.WriteLine("========Send to Master");
+            Console.WriteLine("========Send to Master");
         }
     }
 }
