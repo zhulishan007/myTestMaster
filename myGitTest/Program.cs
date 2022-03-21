15df4c2e97284729e9638c2cfeaa1f604d40d4fa
zhulishan007 <zhulishan007@163.com>
2022-03-21 13:24:37 +0800

    ttt

--- myGitTest/Program.cs
+++ myGitTest/Program.cs
@@ -6,6 +6,7 @@ namespace myGitTest
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("---------------------------");
             Console.WriteLine("Send to Master1");
             Console.WriteLine("Send to Master2");
             Console.WriteLine("Send to Master");
