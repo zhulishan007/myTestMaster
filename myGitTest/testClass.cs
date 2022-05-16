9ff5bf4b12f17f1b9d57d0556b1d8dc2c9ea5cdb
zhulishan007@163.com <zhulishan007@163.com>
2022-05-17 00:16:24 +0800

    21

--- myGitTest/testClass.cs
+++ myGitTest/testClass.cs
@@ -6,10 +6,15 @@ namespace myGitTest
 {
     class testClass
     {
-        int clone;
+        int x = 30;
+        int clone=300;
         public testClass()
-        { 
-        
+        {
+            clone = 300;
+        }
+        public testClass(int cl)
+        {
+            clone = cl;
         }
     }
 }
