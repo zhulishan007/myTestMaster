cd7b29f4d3c8edfc2a17b9f75f0c81fb05384f53
zhulishan007@163.com <zhulishan007@163.com>
2022-11-17 18:15:51 +0800

    master brach

---
+++ myGitTest/testClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace myGitTest
+{
+    class testClass
+    {
+        int x = 30;
+        int clone = 300;
+        public testClass()
+        {
+            Console.WriteLine("main branch");
+            clone = 300;
+        }
+        public testClass(int cl)
+        {
+            clone = cl;
+        }
+
+    }
+}
